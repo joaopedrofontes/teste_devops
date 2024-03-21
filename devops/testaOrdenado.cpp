fc2133161a425de127188942ae0fd11661a82667
João Fontes <joaopedrofonttes@gmail.com>
2024-03-21 14:36:18 -0300

    commit com listaMain

--- devops/testaOrdenado.cpp
+++ devops/testaOrdenado.cpp
@@ -14,12 +14,17 @@ bool testeOrdenado(int lista[], int n) {
 
 int main() {
     int lista[5] = {2, 40, 41, 54, 50};
-    
+    int listaMain[5] = {10, 22, 45, 23, 4};
+
     if (testeOrdenado(lista, 5)) {
-        cout << "Está ordenado" << endl;
+        cout << " A 1a Está ordenada" << endl;
     } else {
-        cout << "Não está ordenado" << endl;
+        cout << "A 1a não está ordenada" << endl;
     }
-    
+    if (testeOrdenado(lista, 5)) {
+        cout << "A listamin está ordenada" << endl;
+    } else {
+        cout << "A lista main não está ordenada" << endl;
+    }
     return 0;
 }
