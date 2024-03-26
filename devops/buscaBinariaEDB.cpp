a8d9129d3fb122f3326d7c483790d26b32808f52
João Fontes <joaopedrofonttes@gmail.com>
2024-03-26 14:17:35 -0300

    adiÇÃo do arquivo de busca binÁria

---
+++ devops/buscaBinariaEDB.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int busca_binaria(int k, int colecao[], int tamanho){
+    int inicio = 0;
+    int fim = tamanho - 1;
+    int meio;
+
+    while(inicio <= fim){
+        meio = (inicio + fim) / 2;
+        if(k < colecao[meio]){
+            fim = meio - 1;
+        }
+        else if(k > colecao[meio]){
+            inicio = meio + 1;
+        }
+        else{
+            return meio;
+        }
+    }
+    return -1;
+}
+
+int main(){
+
+    int tamanho = 7;
+
+    int lista[tamanho] = {2, 4, 35, 77, 102, 300, 301};
+
+    int valor = 301;
+
+    int posicao = busca_binaria(valor, lista, tamanho);
+
+    std::cout << "O valor está na posição " << posicao + 1<< std::endl;
+
+    return 0;
+
+}
