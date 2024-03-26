bc4232f1f0b506e6b15b711af7e859d4d8d15d30
João Fontes <joaopedrofonttes@gmail.com>
2024-03-26 14:28:21 -0300

    adiÇÃo do arquivo funcoes

---
+++ devops/funcoes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int somaDeDoisNumeros(int x, int y){
+    return x + y;
+}
+
+
+
+int main(){
+    
+    int valor1 = 10;
+
+    int valor2 = 4;
+
+    int soma = somaDeDoisNumeros(valor1, valor2);
+
+    return 0;
+
+}
